3286b4737c3e1bcdb630a1521ac9efdc18a8c02a
unknown <Sebastian@SEBASTIAN-DTW10.slowak.cc>
2017-07-03 22:17:09 +0200

    all

---
+++ zxing/zxing/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace zxing
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            var scanPage = new ZXingScannerPage();
+
+            scanPage.OnScanResult += (result) => {
+                // Stop scanning
+                scanPage.IsScanning = false;
+
+                // Pop the page and show the result
+                Device.BeginInvokeOnMainThread(() => {
+                    Navigation.PopAsync();
+                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+            Navigation.PushAsync(scanPage);
+        }
+    }
+}
